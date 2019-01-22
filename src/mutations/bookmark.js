a243c33b30e247593d24dc8793c2d16c301e342b
Arcaelyx <arcaelyx@protonmail.com>
2019-01-21 22:41:04 -0500

    Provide a router method to save bookmarks
    In order to receive POST requests to save bookmarks & achieve looser
    coupling against `express`, a method is provided that takes a req/res
    interface.
    
    The path in which it'll be used is deferred, although in the future,
    further encapsulation from req/res should be provided.

---
+++ src/mutations/bookmark.js
@@ -0,0 +1,5 @@
+import { bookmark } from '../entities'
+
+export const saveBookmark = ({ body: { key = '', url = '' } }, res) =>
+  bookmark.write({ key, url })
+    .then(() => res.status(201).send())

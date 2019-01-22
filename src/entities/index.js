fd9f6b198a12e0cb2c322f65aba0d1b714496ef3
Arcaelyx <arcaelyx@protonmail.com>
2019-01-21 22:16:43 -0500

    Define database entity for bookmark additions
    In order to store bookmarks into the database in a way that's decoupled
    from knex, a bookmark entity is defined that makes use of a simple
    read/write interface to be used across the application codebase.

--- src/entities/index.js
+++ src/entities/index.js
@@ -2,3 +2,5 @@ import config from '../../config.json'
 import entityMap from '../utils/entityMap'
 
 const entityMapper = entityMap(config)
+
+export const bookmark = entityMapper.define('bookmark', ['key', 'url'])

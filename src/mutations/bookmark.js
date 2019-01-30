735bb9cc24dbe459eaa10d0b86eab8ff824b8813
beingbrown <randy@beingbrown.net>
2019-01-30 01:37:01 +0000

    catch errors and send to handler middleware
    if write fails, the client hangs, because it never gets a response. At
    present, failure is the default behavior because there's no initialized
    db in the container. In order to facilitate a communication with the
    client, the error has to be caught.

--- src/mutations/bookmark.js
+++ src/mutations/bookmark.js
@@ -3,3 +3,4 @@ import { bookmark } from '../entities'
 export const saveBookmark = ({ body: { key = '', url = '' } }, res) =>
   bookmark.write({ key, url })
     .then(() => res.status(201).send())
+    .catch(next)

04db0c182778a552cf3a8be65a0cafba2fa575f8
beingbrown <randy@beingbrown.net>
2019-02-06 01:14:49 +0000

    define a prototypical writeBoundary
    because this is not finished, it is not exposed for consumption. But,
    because field definitions are leveraged in both the boundary and the
    entity (though not necessary here), there is an opportunity to reduce
    the sources of truth. This does have the potential side effect of making
    the resource the coupling point for everything. The architecture still
    needs a little fleshing out.
    
    The prototype takes a controller and a ruleset and leverages the
    controller write on the result of the ruleset. This means that there's
    an expectation of either assumed order of the rules or unified contract.

--- src/boundary/index.js
+++ src/boundary/index.js
@@ -1,3 +1,6 @@
 import { bookmark } from '../entities'
 
+const writeBoundary = (controller, rules = [e => e]) => obj =>
+  controller.write(rules.reduce((acc, fn) => (fn(acc)), obj))
+
 export default saveBookmark = ({ key, url }) => bookmark.write({ key, url })

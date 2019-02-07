6527d6b8260993d9f692dc5e3e23232cefb53658
beingbrown <randy@beingbrown.net>
2019-02-07 03:01:51 +0000

    provide a read method in the boundary
    in order to be able to read the bookmarks in the current structure, the
    boundary needs to expose read access to the entity. the shift to the new
    boundary strategy will eliminate this need.

--- src/boundary/index.js
+++ src/boundary/index.js
@@ -3,4 +3,5 @@ import { bookmark } from '../entities'
 const writeBoundary = (controller, rules = [e => e]) => obj =>
   controller.write(rules.reduce((acc, fn) => (fn(acc)), obj))
 
-export default saveBookmark = ({ key, url }) => bookmark.write({ key, url })
+export readBookmarks = () => bookmark.read()
+export saveBookmark = ({ key, url }) => bookmark.write({ key, url })

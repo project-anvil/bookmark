83cfb1c0002d442679e5041c21dfdae9bab8ef2f
beingbrown <randy@beingbrown.net>
2019-02-08 01:08:06 +0000

    leverage boundary read at the resource
    the current structure of the boundary requires traditional n-tier
    treatment, meaning that the resource will leverage the boundary in order
    to read. In order for this to be ready for production, it needs to
    comply with a contract in the absence of data.

--- src/resource/bookmark.js
+++ src/resource/bookmark.js
@@ -1,4 +1,7 @@
-import { saveBookmark } from '../boundary'
+import { saveBookmark, readBookmarks } from '../boundary'
+
+const readBookmarks = (req, res, next) =>
+  readBookmarks().then(data => res.status(200).send(data)).catch(next)
 
 export default bookmark = (req, res, next) =>
   saveBookmark(req.body)

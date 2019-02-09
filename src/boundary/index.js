0a6b2544c5c449a8769faba3916835199213ea00
beingbrown <randy@beingbrown.net>
2019-02-09 22:42:56 +0000

    expose two boundary interfaces for bookmarks
    the boundary has to expose both a read and a write interface. Without
    the const keyword, the export does not behave as expected, which was
    causing the app to fail to build.

--- src/boundary/index.js
+++ src/boundary/index.js
@@ -3,5 +3,5 @@ import { bookmark } from '../entities'
 const writeBoundary = (controller, rules = [e => e]) => obj =>
   controller.write(rules.reduce((acc, fn) => (fn(acc)), obj))
 
-export readBookmarks = () => bookmark.read()
-export saveBookmark = ({ key, url }) => bookmark.write({ key, url })
+export const readBookmarks = () => bookmark.read()
+export const saveBookmark = ({ key, url }) => bookmark.write({ key, url })

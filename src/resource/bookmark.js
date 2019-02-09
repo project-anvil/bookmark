b6efabf0ec26dc90b7d814a73fdbd6b0d819184c
beingbrown <randy@beingbrown.net>
2019-02-09 22:47:20 +0000

    expose a read and write interface in the resource
    this is a near mapping of the boundary, which trends toward the problems
    of n-tier. The preferred use case would be to leverage a more dynamic
    boundary that can be provided rules and entities as a part of its
    creation.
    
    In addition, the names of these methods leave something to be desired.
    The output of this could be a collectio of {endpoint, http_method,
    procesor} which could either be provided directly to express or
    leveraged in a router.
    
    As it is, these methods are just request masks on the boundary.

--- src/resource/bookmark.js
+++ src/resource/bookmark.js
@@ -1,9 +1,10 @@
 import { saveBookmark, readBookmarks } from '../boundary'
 
-const readBookmarks = (req, res, next) =>
-  readBookmarks().then(data => res.status(200).send(data)).catch(next)
+export const retrieve = (req, res, next) =>
+  readBookmarks().then(data => res.status(200).send(data))
+    .catch(next)
 
-export default bookmark = (req, res, next) =>
+export const bookmark = (req, res, next) =>
   saveBookmark(req.body)
     .then(() => res.status(201).send())
     .catch(next)

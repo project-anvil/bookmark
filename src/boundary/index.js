a27ba825f1cb98c932c34696714d5c7ea709d6d0
randy brown <randy@beingbrown.net>
2019-02-11 21:37:34 -0600

    abstract the boundary away from a specific mechanism
    by allowing the end mechanism to be passed in with the applicable
    ruleset, the specifics of the controller are no longer required
    to be known by the boundary.

--- src/boundary/index.js
+++ src/boundary/index.js
@@ -1,7 +1,7 @@
 import { bookmark } from '../entities'
 
-const writeBoundary = (controller, rules = [e => e]) => obj =>
-  controller.write(rules.reduce((acc, fn) => (fn(acc)), obj))
+export const boundary = (behavior, rules = [e => e]) => obj =>
+  behavior(rules.reduce((acc, fn) => fn(acc), obj))
 
 export const readBookmarks = () => bookmark.read()
 export const saveBookmark = ({ key, url }) => bookmark.write({ key, url })

87586421932e7e5906bfba014b9dd77d20557bb9
randy brown <randy@beingbrown.net>
2019-02-13 18:37:56 -0600

    integrate the resource with the server
    because the endpoints have migrated in the resource, they have to
    be updated in the server.

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import morgan from 'morgan'
 import logger from './utils/logger'
 import { stackTraceMask } from './utils'
 
-import { retrieve, bookmark } from './resource/bookmark'
+import { read, write } from './resource/bookmark'
 
 const { PORT = 80 } = process.env
 
@@ -15,7 +15,7 @@ app.use(morgan('combined', { stream: logger.stream }))
 app.use(bodyParser.json())
 app.use(stackTraceMask(logger))
 
-app.post('/', bookmark)
-app.get('/', retrieve)
+app.post('/', write)
+app.get('/', read)
 
 app.listen(PORT)

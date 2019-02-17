f75c142a39cc12691de576cd25fce07e6b592c8e
randy brown <randy@beingbrown.net>
2019-02-17 16:33:54 -0600

    disable boundary behaviors bound to explicit entity
    because the more general boundary model is being leveraged, the
    binding to a single entity is no longer needed.

--- src/boundary/index.js
+++ src/boundary/index.js
@@ -1,7 +1,2 @@
-import { bookmark } from '../entities'
-
 export const boundary = (behavior, rules = [e => e]) => obj =>
   behavior(rules.reduce((acc, fn) => fn(acc), obj))
-
-export const readBookmarks = () => bookmark.read()
-export const saveBookmark = ({ key, url }) => bookmark.write({ key, url })

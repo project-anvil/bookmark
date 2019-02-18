3505ffdc94a4d0ca5f50c90f64b6726b9127af6e
randy brown <randy@beingbrown.net>
2019-02-18 17:48:23 -0600

    manage all routes with middleware
    a router middleware is injected for the resources collection
    defined by the bookmarks resource. In addition, a 404 middleware
    is injected to ensure any unmapped routes are handled by the error
    handler.

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import morgan from 'morgan'
 import logger from './utils/logger'
 import { stackTraceMask } from './utils'
 
-import { read, write } from './resource/bookmark'
+import { read, write, resources } from './resource/bookmark'
 
 const { PORT = 80 } = process.env
 
@@ -21,9 +21,12 @@ const makeRouter = behaviors => {
 
 app.use(morgan('combined', { stream: logger.stream }))
 app.use(bodyParser.json())
-app.use(stackTraceMask(logger))
 
-app.post('/', write)
-app.get('/', read)
+resources.forEach(({ resource, behaviors }) =>
+  app.use(resource, makeRouter(behaviors)))
+
+app.use((req, res, next) => next({status: 404}))
+
+app.use(stackTraceMask(logger))
 
 app.listen(PORT)

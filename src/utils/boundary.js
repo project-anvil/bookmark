b14b61174bedca75cbf09cd99fc2620aadebe05f
randy brown <randy@beingbrown.net>
2019-03-26 18:35:09 -0500

    provide a pattern for ingress and egress transformation
    the current boundary always applies the controller method and returns
    what is produced by the controller. However, it is possible that an
    egress mapper will need to strip off attributes or make modifications
    on the returned object as well.
    
    For the near term, the boundary will act as a wrapper on the composer.

--- src/utils/boundary.js
+++ src/utils/boundary.js
@@ -1,2 +1,4 @@
 export const boundary = (behavior, rules = [e => e]) => obj =>
   behavior(rules.reduce((acc, fn) => fn(acc), obj))
+
+export const composer = (...fns) => obj => fns.reduce((acc, fn) => fn(acc), obj))

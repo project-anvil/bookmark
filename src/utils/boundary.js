b9059f41465c63008cd41732ae29135ccca4721f
randy brown <randy@beingbrown.net>
2019-07-09 22:53:14 -0500

    remove boilerplate code
    bookmarks should focus primarily on the entity relationship that it
    represents as a module, which can be injected into a server or not.

--- src/utils/boundary.js
+++
@@ -1,4 +0,0 @@
-export const boundary = (behavior, rules = [e => e]) => obj =>
-  behavior(rules.reduce((acc, fn) => fn(acc), obj))
-
-export const composer = (...fns) => obj => fns.reduce((acc, fn) => fn(acc), obj))

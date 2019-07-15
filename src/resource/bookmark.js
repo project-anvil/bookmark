afba172e77d786e059d116b0a2fc2ef562e3c3de
randy brown <randy@beingbrown.net>
2019-07-14 22:05:32 -0500

    provide the entire bookmarks resource as a function
    this will migrate away from a resource, eventually, to provide an api
    to interact with--for now it conforms with a resource spec allowing
    injected endpoints.

--- src/resource/bookmark.js
+++ src/resource/bookmark.js
@@ -1,23 +1,24 @@
-import { boundary } from '../utils/boundary'
-import { bookmark } from '../entities'
+export default ({boundary, logger, entityMaper}) => {
+  const bookmark = entityMapper.define('bookmark', ['key', 'url'])
 
-const writeBookmark = boundary(bookmark.write)
-const readBookmarks = boundary(bookmark.read)
+  const writeBookmark = boundary(bookmark.write)
+  const readBookmarks = boundary(bookmark.read)
 
-const read = (req, res, next) =>
-  readBookmarks().then(data => res.status(200).send(data))
-    .catch(next)
+  const read = (req, res, next) =>
+    readBookmarks().then(data => res.status(200).send(data))
+      .catch(next)
 
-const write = (req, res, next) =>
-  writeBookmark(req.body)
-    .then(() => res.status(201).send())
-    .catch(next)
+  const write = (req, res, next) =>
+    writeBookmark(req.body)
+      .then(() => res.status(201).send())
+      .catch(next)
 
-export const resources = [
-  { resource: "/bookmarks",
-    behaviors: [
-      {endpoint: "/", method: "get", behavior: read},
-      {endpoint: "/", method: "post", behavior: write}
-    ]
-  }
-];
+  return [
+    { resource: "/bookmarks",
+      behaviors: [
+        {endpoint: "/", method: "get", behavior: read},
+        {endpoint: "/", method: "post", behavior: write}
+      ]
+    }
+  ];
+};

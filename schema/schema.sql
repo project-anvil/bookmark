ab1d23598886ac55a03175139851da4f44786565
randy brown <randy@beingbrown.net>
2019-07-15 22:31:09 -0500

    simplify the application structure
    in the new world, the app does not need to dictate a schema, and entities
    are incorporated into the bookmark producer. The actual bookmark object
    will be refactored to better align with a more universal tool, which can
    then be leveraged in a service or a terminal tool.

--- schema/schema.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE bookmark (
-  key TEXT NOT NULL,
-  url TEXT NOT NULL,
-  date_created INTEGER DEFAULT (strftime('%s', 'now'))
-);
--- src/entities/index.js
+++
@@ -1,6 +0,0 @@
-import config from '../../config.json'
-import entityMap from '../utils/entityMap'
-
-const entityMapper = entityMap(config)
-
-export const bookmark = entityMapper.define('bookmark', ['key', 'url'])

636b038462a6f5ef4122fc10fb653581cbceacef
randy brown <randy@beingbrown.net>
2019-07-18 23:13:05 -0500

    reduce the return to the interaction with the boundary
    in order to provide an interface that can be managed through multiple
    toolsets, the endpoint focused code is removed--it will be provided
    elsewhere.

--- src/bookmark.js
+++ src/bookmark.js
@@ -1,24 +1,8 @@
 export default ({boundary, logger, entityMaper}) => {
-  const bookmark = entityMapper.define('bookmark', ['key', 'url'])
+  const {read, write} = entityMapper.define('bookmark', ['key', 'url'])
 
-  const writeBookmark = boundary(bookmark.write)
-  const readBookmarks = boundary(bookmark.read)
-
-  const read = (req, res, next) =>
-    readBookmarks().then(data => res.status(200).send(data))
-      .catch(next)
-
-  const write = (req, res, next) =>
-    writeBookmark(req.body)
-      .then(() => res.status(201).send())
-      .catch(next)
-
-  return [
-    { resource: "/bookmarks",
-      behaviors: [
-        {endpoint: "/", method: "get", behavior: read},
-        {endpoint: "/", method: "post", behavior: write}
-      ]
-    }
-  ];
+  return {
+    read,
+    write
+  };
 };
